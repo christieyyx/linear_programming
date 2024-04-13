f3e294f9a661089e994360c0e529616a0b220609
christieyyx <yyang46@uw.edu>
2024-04-13 13:02:53 -0700

    mulvec solved

--- MyProject/linear_progr.lean
+++ MyProject/linear_progr.lean
@@ -1,3 +1,11 @@
-import Mathlib.Topology.Basic
+import Mathlib.Analysis.InnerProductSpace.PiL2
+import Mathlib.Data.Matrix.Basic
+import Mathlib.Data.Matrix.Reflection
 
-#check TopologicalSpace
+open Matrix
+variable(m n :ℕ)
+variable (M : Matrix (Fin m) (Fin n) ℝ) (v : (Fin n) → ℝ)
+#check M *ᵥ v  -- works
+
+--#check (Matrix (Fin m) (Fin n) k) *ᵥ x
+--
