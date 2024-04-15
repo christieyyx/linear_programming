81a63e5c5604c9107893ad91cbd9d1e4f355320d
christieyyx <yyang46@uw.edu>
2024-04-15 13:17:59 -0700

    check basics

--- MyProject/linear_progr.lean
+++ MyProject/linear_progr.lean
@@ -1,11 +1,13 @@
 import Mathlib.Analysis.InnerProductSpace.PiL2
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Matrix.Reflection
+import Mathlib.Data.Matrix.RowCol
 
 open Matrix
 variable(m n :ℕ)
-variable (M : Matrix (Fin m) (Fin n) ℝ) (v : (Fin n) → ℝ)
+variable (M : Matrix (Fin m) (Fin n) ℝ) (v : (Fin n) → ℝ)(v₁: (Fin m) → ℝ )(i: Fin m)
 #check M *ᵥ v  -- works
-
---#check (Matrix (Fin m) (Fin n) k) *ᵥ x
---
+#check row v
+#check col v
+#check (row v₁) * M
+#check M i
